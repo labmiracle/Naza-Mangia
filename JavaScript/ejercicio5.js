77c542e49968aaa291a8d70ed4e97e5abc5ce934
NazaMangia <naza.mangia@gmail.com>
2023-04-21 17:12:02 -0300

    ex 5 added

---
+++ JavaScript/ejercicio5.js
@@ -0,0 +1,11 @@
+function camelize(str) {
+    const words = str.split(' ');
+    words[0] = words[0].toLowerCase();
+
+    for (let i = 1; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+
+    }
+
+    return words.join('')
+}
